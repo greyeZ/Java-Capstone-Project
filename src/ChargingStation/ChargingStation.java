add5ab029f43043d0517f4fa131b463007fe4f12
danja <dani.janzen12@web.de>
2023-12-04 16:52:46 +0100

    Queueing System

--- src/ChargingStation/ChargingStation.java
+++ src/ChargingStation/ChargingStation.java
@@ -3,117 +3,148 @@ package ChargingStation;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import Car.Car;
 import Weather.WeatherCondition;
 
 public class ChargingStation {
-	private Location[] _locations;
-	private int _id; 
-	private WeatherCondition _currentWeather;
-	
-	
-	public ChargingStation(int amountOfLocations, int id) {
-		this._locations = new Location[amountOfLocations];
-		fillLocationArray(amountOfLocations);
-		this._id = id;
-		createLogFile();
-	}
+    private Location[] _locations;
+    private int _id;
+    private WeatherCondition _currentWeather;
+    private final BlockingQueue<Car> queue = new LinkedBlockingQueue<>();
 
-	private void fillLocationArray(int amountOfLocations) {
-		for(int i=0; i < amountOfLocations; i+=1) {
-			get_locations()[i] = new Location();
-		}
-	}
+    public ChargingStation(int amountOfLocations, int id) {
+        this._locations = new Location[amountOfLocations];
+        fillLocationArray(amountOfLocations);
+        this._id = id;
+        createLogFile();
+        startCharging();
+    }
 
-	public Location[] get_locations() {
-		return _locations;
-	}
+    private void fillLocationArray(int amountOfLocations) {
+        for (int i = 0; i < amountOfLocations; i += 1) {
+            get_locations()[i] = new Location();
+        }
+    }
 
-	public int get_id() {
-		return _id;
-	}
+    public Location[] get_locations() {
+        return _locations;
+    }
 
-	public void set_id(int _id) {
-		this._id = _id;
-	}
+    public int get_id() {
+        return _id;
+    }
 
-	public WeatherCondition get_currentWeather() {
-		return _currentWeather;
-	}
+    public void set_id(int _id) {
+        this._id = _id;
+    }
 
-	public void set_currentWeather(WeatherCondition _currentWeather) {
-		this._currentWeather = _currentWeather;
-		changeEnergySource();
-		logChanges("Weather changed to: " + _currentWeather);
-	}
-	
-	public void useChargingLocation(Car car) {
-		for(int i=0; i < get_locations().length; i+=1) {
-			if(!get_locations()[i].is_inUse()) {
-				get_locations()[i].set_inUse(true);
-				get_locations()[i].set_carInLocation(car);
-				car.set_charging(true);
-			}
-		}
-		if(!car.is_charging()) {
-			for(int i=0; i < get_locations().length; i+=1) {
-				//TODO Logic for checking if there is an available Location
-			}
-		}
-	}
-	
-	private void changeEnergySource() {
-		switch (get_currentWeather()) {
-		    case SUNNY:
-		    	updateEnergySourceForLocation(EnergySource.SOLAR);
-		        break;
-		    case WINDY:
-		    	updateEnergySourceForLocation(EnergySource.WIND);
-		        break;
-		    case RAINING:
-		    	updateEnergySourceForLocation(EnergySource.ELECTRICITY);
-		        break;
-		    case CLOUDY:
-		    	updateEnergySourceForLocation(EnergySource.WIND);
-		        break;
-		    case SNOWING:
-		    	updateEnergySourceForLocation(EnergySource.GAS);
-		        break;
-		    default:
-		    	updateEnergySourceForLocation(EnergySource.SOLAR);
-		}
+    public WeatherCondition get_currentWeather() {
+        return _currentWeather;
+    }
 
-	}
-	
-	private void updateEnergySourceForLocation(EnergySource newSource) {
-		for(Location location: get_locations()) {
-			location.set_energySource(newSource);
-		}
-		logChanges("EnergySource changed to: " + newSource);
-	}
-	
-	private void logChanges(String text) {
-		System.out.println("Writing to file...");
-		//TODO Logic for writing to existing file, currently overwritng the old one
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter("../../ChargingStation" + get_id() + "log.txt"));
-			writer.newLine();
-			writer.write(text);
-			writer.close();
-		} catch(IOException e) {
-			 e.printStackTrace();
-			 System.out.println("Error writing to file");
-		}
-	}
-	
+    public void set_currentWeather(WeatherCondition _currentWeather) {
+        this._currentWeather = _currentWeather;
+        changeEnergySource();
+        logChanges("Weather changed to: " + _currentWeather);
+    }
+
+    public void useChargingLocation(Car car) {
+        for (int i = 0; i < get_locations().length; i += 1) {
+            if (!get_locations()[i].is_inUse()) {
+                get_locations()[i].set_inUse(true);
+                get_locations()[i].set_carInLocation(car);
+                car.set_charging(true);
+                return; // Exit the method if a location is successfully assigned
+            }
+        }
+        addVehicleToQueue(car); // No available location, add to the queue
+    }
+
+    private void addVehicleToQueue(Car car) {
+        queue.add(car);
+    }
+
+    private void startCharging() {
+        for (int i = 0; i < get_locations().length; i += 1) {
+            final int stationIndex = i;
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        Car car = queue.take(); 
+                        get_locations()[stationIndex].set_inUse(true);
+                        get_locations()[stationIndex].set_carInLocation(car);
+                        car.set_charging(true);
+                        chargeVehicle(car);
+                        get_locations()[stationIndex].set_inUse(false);
+                        get_locations()[stationIndex].set_carInLocation(null);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+            }).start();
+        }
+    }
+
+
+    private void chargeVehicle(Car car) throws InterruptedException {
+        Thread.sleep((long) car.getChargingTime()); 
+        System.out.println("Vehicle " + car.get_id() + " charged at ChargingStation " + get_id());
+        car.set_charging(false);
+    }
+
+    private void changeEnergySource() {
+        switch (get_currentWeather()) {
+            case SUNNY:
+                updateEnergySourceForLocation(EnergySource.SOLAR);
+                break;
+            case WINDY:
+                updateEnergySourceForLocation(EnergySource.WIND);
+                break;
+            case RAINING:
+                updateEnergySourceForLocation(EnergySource.ELECTRICITY);
+                break;
+            case CLOUDY:
+                updateEnergySourceForLocation(EnergySource.WIND);
+                break;
+            case SNOWING:
+                updateEnergySourceForLocation(EnergySource.GAS);
+                break;
+            default:
+                updateEnergySourceForLocation(EnergySource.SOLAR);
+        }
+    }
+
+    private void updateEnergySourceForLocation(EnergySource newSource) {
+        for (Location location : get_locations()) {
+            location.set_energySource(newSource);
+        }
+        logChanges("EnergySource changed to: " + newSource);
+    }
+
+    private void logChanges(String text) {
+        System.out.println("Writing to file...");
+        // TODO Logic for writing to existing file, currently overwriting the old one
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("ChargingStation" + get_id() + "log.txt", true));
+            writer.newLine();
+            writer.write(text);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error writing to file");
+        }
+    }
+
     private void createLogFile() {
         try {
-        	BufferedWriter writer = new BufferedWriter(new FileWriter("ChargingStation" + get_id() + "log.txt"));
-            
+            BufferedWriter writer = new BufferedWriter(new FileWriter("ChargingStation" + get_id() + "log.txt"));
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-	
 }
--- src/JavaCapstoneProject.java
+++ src/JavaCapstoneProject.java
@@ -2,50 +2,80 @@ import java.util.Random;
 
 import Car.Car;
 import ChargingStation.ChargingStation;
+import ChargingStation.Location;
 import Weather.WeatherCondition;
 
 public class JavaCapstoneProject {
 
-	public static void main(String[] args) {
-		ChargingStation[] chargingStations = {
-		new ChargingStation(5, 1111),
-		new ChargingStation(7, 2222),
-		new ChargingStation(4, 3333)
-		};
-		
-		Car[] cars = {
-			new Car(11, 3, 2), 
-			new Car(12, 1, 1), 
-			new Car(13, 4, 3), 
-			new Car(14, 2, 3), 
-			new Car(15, 5, 5)
-		};
-		for(ChargingStation chargingStation: chargingStations) {
-			chargingStation.set_currentWeather(changeWeather());
-		}
-		
-		//TODO Car is using a Location of a CHargingStation
-	}
-	
-	static WeatherCondition changeWeather() {
-		 // Create a Random object
+    public static void main(String[] args) {
+        ChargingStation[] chargingStations = {
+            new ChargingStation(5, 1111),
+        };
+
+        for (ChargingStation chargingStation : chargingStations) {
+            chargingStation.set_currentWeather(changeWeather());
+        }
+
+        Car[] cars = {
+            new Car(11, 3, 2),
+            new Car(12, 1, 1),
+            new Car(13, 4, 3),
+            new Car(14, 2, 3),
+            new Car(15, 5, 5),
+            new Car(16, 4, 3),
+            new Car(17, 2, 3),
+            new Car(18, 5, 5),
+            new Car(19, 3, 2),
+            new Car(20, 1, 1),
+            new Car(21, 4, 3),
+            new Car(22, 2, 3),
+            new Car(23, 5, 5),
+            new Car(24, 4, 3),
+            new Car(25, 2, 3),
+        };
+
+        // Simulate cars using the charging stations
+        for (Car car : cars) {
+            ChargingStation selectedStation = findAvailableChargingStation(chargingStations);
+            if (selectedStation != null) {
+                selectedStation.useChargingLocation(car);
+                System.out.println("Car " + car.get_id() + " is charging at ChargingStation " + selectedStation.get_id());
+            } else {
+                System.out.println("No available charging station for Car " + car.get_id());
+            }
+        }
+    }
+
+    static WeatherCondition changeWeather() {
+        // Create a Random object
         Random random = new Random();
 
         // Generate a random number between 1 and 5 (inclusive)
-        int randomNumber = random.nextInt(5) + 1; 
-        switch(randomNumber) {
-        case 1: 
-        	return WeatherCondition.CLOUDY;
-        case 2: 
-        	return WeatherCondition.RAINING;
-        case 3: 
-        	return WeatherCondition.SNOWING;
-        case 4: 
-        	return WeatherCondition.SUNNY;
-        case 5: 
-        	return WeatherCondition.WINDY;
-        default: return WeatherCondition.SUNNY;
+        int randomNumber = random.nextInt(5) + 1;
+        switch (randomNumber) {
+            case 1:
+                return WeatherCondition.CLOUDY;
+            case 2:
+                return WeatherCondition.RAINING;
+            case 3:
+                return WeatherCondition.SNOWING;
+            case 4:
+                return WeatherCondition.SUNNY;
+            case 5:
+                return WeatherCondition.WINDY;
+            default:
+                return WeatherCondition.SUNNY;
         }
-	}
+    }
 
+    static ChargingStation findAvailableChargingStation(ChargingStation[] chargingStations) {
+        for (ChargingStation chargingStation : chargingStations) {
+            for (Location location : chargingStation.get_locations()) {
+                if (!location.is_inUse()) {
+                    return chargingStation;
+                }
+            }
+        }
+        return null;
+    }
 }
